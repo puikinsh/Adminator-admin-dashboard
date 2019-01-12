bb758ed116e0a43f31245271bf9d66c0f9f5910d
Francesca Sperati <6431@ait.nsw.edu.au>
2019-01-12 18:26:06 +1100

    adjusting size of boxes

--- src/assets/scripts/charts/easyPieChart/index.js
+++ src/assets/scripts/charts/easyPieChart/index.js
@@ -4,6 +4,10 @@ import 'easy-pie-chart/dist/jquery.easypiechart.min.js';
 export default (function () {
   if ($('.easy-pie-chart').length > 0) {
     $('.easy-pie-chart').easyPieChart({
+      lineWidth:7,
+      lineCap:"square",
+      scaleColor:false,
+      animate: 2500,
       onStep(from, to, percent) {
         this.el.children[0].innerHTML = `${Math.round(percent)} %`;
       },
