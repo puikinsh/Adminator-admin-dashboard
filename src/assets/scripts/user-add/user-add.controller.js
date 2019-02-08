40efa03db556e7c37d2e4852ce8e77a33e1e90e6
Francesca Sperati <6431@ait.nsw.edu.au>
2019-02-08 19:47:54 +1100

    referee user-add

--- src/assets/scripts/user-add/user-add.controller.js
+++ src/assets/scripts/user-add/user-add.controller.js
@@ -1,10 +1,20 @@
 /** @ngInject */
-const UserAddController = ($scope) => {
+const UserAddController = ($scope,UserService) => {
     $scope.$on('loadUserSuccess', function (event, user) {
         $scope.user = user;
 
         $scope.classifications = ['class1', 'class2'];
         $scope.skills = ['s1', 's2'];
+
+        $scope.tab = 1;
+
+        $scope.setTab = (newTab) => {
+            $scope.tab = newTab;
+        }
+
+        $scope.isSet = (tabNum) => {
+            return $scope.tab === tabNum;
+        }
 
     });
     UserService.loadUser();
