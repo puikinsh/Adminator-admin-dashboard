9158609d46536939f222826d010bec8efbfbeb4c
Mohammed Tantawy <6102@ait.nsw.edu.au>
2019-02-10 00:28:54 +1100

    Modified topbar to get first char of user

--- src/assets/scripts/nwTopbar/topbar.controller.js
+++ src/assets/scripts/nwTopbar/topbar.controller.js
@@ -8,20 +8,19 @@ import masonry from '../masonry';
 const TopbarController = ($scope, UserService) => {
     $scope.$on('loadUserSuccess', function (event, user) {
         $scope.user = user;
-    });
 
-    $scope.avatar = new Avatar(document.getElementById('avatar'), {
-        'useGravatar': false,
-        'initials': 'AD', // it will be`${$scope.user.first_name[0]}${$scope.user.last_name[1]}`
-        'initial_weight': 300,
-    });
+        $scope.avatar = new Avatar(document.getElementById('avatar'), {
+            'useGravatar': false,
+            'initials': user.name[0],
+            'initial_weight': 300,
+        });
 
-    $scope.$on('needReload', function(event){
-        console.log("NEED RELOAD!");
-        functionSearch();
-        masonry();
+        $scope.$on('needReload', function(event){
+            console.log("NEED RELOAD!");
+            functionSearch();
+            masonry();
+        });
     });
-    
     UserService.loadUser();
     
 
