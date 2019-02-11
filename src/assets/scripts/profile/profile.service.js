9fa98f234f018391734ebd679034fd3d7741a48e
Mohammed Tantawy <6102@ait.nsw.edu.au>
2019-02-11 18:57:52 +1100

    profile upload start

--- src/assets/scripts/profile/profile.service.js
+++ src/assets/scripts/profile/profile.service.js
@@ -7,6 +7,12 @@ const ProfileService = ($http) => {
         .then( (result) => { return result.data; })
         .catch( (error) => { return error; });
     }
+
+    serv.updateUserProfile = (id) => {
+        return $http.get(`${process.env.RESTAPI_URL}/api/user/${id}`, { withCredentials: true })
+        .then( (result) => { return result.data; })
+        .catch( (error) => { return error; });
+    }
     return serv;
 }
 
